815f20f08ba33ac68677e8e065c50e313c789bf1
nomints242 <nomints242@gmail.com>
2022-01-19 17:19:18 +0800

    feat: apply

--- src/widgets/apply/index.tsx
+++ src/widgets/apply/index.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@mui/material";
+import ShowMoreText from "react-show-more-text";
 
 const ApplyWidget = () => {
   return (
     <>
       <form>
-        <Grid container direction="column" alignItems="flex-start">
+        <label>
+          <input type="radio"></input>
+          <h3>Si Plus</h3>
+          <label>
+            Pay for your new Mac over 12 months at 0% APR with Apple Card.* Just
+            choose Apple Card Monthly Installments when you check out to apply.
+          </label>
+          <p>OS</p>
+          <label>Android 7.1 Android 7.1 Sunmi OS</label>
+          <p>CPU</p>
+          <label>Quad-core 1.4Hz CPU</label>
+          <p>Memory</p>
+          <label>1GB RAM + 8GB ROM 2GB+16GB ROM (optional)</label>
+          <p>Display</p>
+          <label>5.99" HD+ 1440x720 IPS</label>
+        </label>
+
+        <label>
+          <input type="radio"></input>
+          <h3>Si Pro</h3>
+          <label>
+            Pay for your new Mac over 12 months at 0% APR with Apple Card.* Just
+            choose Apple Card Monthly Installments when you check out to apply.
+          </label>
+          <p>OS</p>
+          <label>Android 7.1 Android 7.1 Sunmi OS</label>
+          <p>CPU</p>
+          <label>Quad-core 1.4Hz CPU</label>
+          <p>Memory</p>
+          <label>1GB RAM + 8GB ROM 2GB+16GB ROM (optional)</label>
+          <p>Display</p>
+          <label>5.99" HD+ 1440x720 IPS</label>
+        </label>
+      </form>
+      <h2>Тохиргоо</h2>
+      <form>
+        <Grid container direction="column" alignItems="flex-start" spacing={3}>
           <Grid item md={12}>
             <input id="Golomt" type="radio" value="Golomt Bank" name="Bank" />
             <label htmlFor="Golomt">Golomt Bank</label>
